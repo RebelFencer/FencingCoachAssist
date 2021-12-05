0c407239412f5b52937fc9e081cd4f5a27f24168
RebelFencer <tomasz.krasnicki@gmail.com>
2021-12-05 18:18:24 +0100

    3 commit - CRUD

--- szermierka/urls.py
+++ szermierka/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from szermierka.views import wszystkie_filmy
+from szermierka.views import wszystkie_filmy, nowy_film
 
 
 urlpatterns = [
-    path('wszystkie/', wszystkie_filmy)
+    path('wszystkie/', wszystkie_filmy),
+    path('nowy/', nowy_film)
+
 ]
--- szermierka/views.py
+++ szermierka/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Film
+from .forms import FilmForm
 
 
 
 
 def wszystkie_filmy(request):
    wszystkie = Film.objects.all()
+   # wszystkie = []
    return render(request, 'filmy.html', {'filmy': wszystkie})
 
+def nowy_film(request):
+   form = FilmForm(request.POST or None, request.FILES or None)
 
+   if form.is_valid():
+      form.save()
+   return render(request, 'nowy_film.html', {'form': form})
+
+
+# Create
+# Read - all, filter, get
+# Upgrade
+# Delete
 
    # return HttpResponse("<h1>To jest nasz pierwszy test</h1>")
    # test = "To jest cos we views"
