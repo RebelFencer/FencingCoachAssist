b6c5895649bde504b0627f3f07cc48071c058a1c
RebelFencer <tomasz.krasnicki@gmail.com>
2021-12-08 00:20:28 +0100

    4 commit - CRUD

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -126,6 +126,10 @@ STATICFILES_DIRS = ['moje_static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'moje_media'
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'wszystkie_filmy'
+LOGOUT_REDIRECT_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- szermierka/views.py
+++ szermierka/views.py
@@ -1,22 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+# from django.http import HttpResponse
 from .models import Film
 from .forms import FilmForm
+from django.contrib.auth.decorators import login_required
 
 
 
+#WYŚWIETLANIE FILMU
 
 def wszystkie_filmy(request):
    wszystkie = Film.objects.all()
    # wszystkie = []
    return render(request, 'filmy.html', {'filmy': wszystkie})
 
+#DODAWANIE FILMU
+@login_required
 def nowy_film(request):
    form = FilmForm(request.POST or None, request.FILES or None)
 
    if form.is_valid():
       form.save()
-   return render(request, 'nowy_film.html', {'form': form})
+      return redirect(wszystkie_filmy)
+
+   return render(request, 'film_form.html', {'form': form})
+
+#EDYTOWANIE FILMU
+@login_required
+def edytuj_film(request, id):
+   film = get_object_or_404(Film, pk=id)
+   form = FilmForm(request.POST or None, request.FILES or None, instance=film)
+
+   if form.is_valid():
+      form.save()
+      return redirect(wszystkie_filmy)
+
+   return render(request, 'film_form.html', {'form': form})
+
+#USUWANIE FILMU
+@login_required
+def usun_film(request, id):
+   film = get_object_or_404(Film, pk=id)
+
+   if request.method == "POST":
+      film.delete()
+      return redirect(wszystkie_filmy)
+
+   return render(request, 'potwierdz.html', {'film': film})
 
 
 # Create
